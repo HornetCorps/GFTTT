6ffdb09de1427c7ab6f8b8cafa2dd484bf300809
RamonCMata <ramoncmata@csus.edu>
2022-02-14 19:03:04 -0800

    Account Registration UI Components
    GFTTT-73

---
+++ src/components/pages/register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../App.css';
+import './register.css';
+export default function Register() {
+    return <div className='register'>
+
+    <h1>Registration</h1>
+    <label>E-mail</label>
+    <input type="text"/>
+    <label>Username</label>
+    <input type="text" />
+    <label>Password</label>
+    <input type="text" />
+    <button>Register</button>
+    
+    
+    
+    
+    
+    </div>
+}
