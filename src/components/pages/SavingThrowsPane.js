712101f728e3c51f51557e8e9c1a0943e636ff90
alaurente21 <74069975+alaurente21@users.noreply.github.com>
2022-04-12 06:43:58 +0000

    Saving Throws Update

--- src/components/pages/SavingThrowsPane.js
+++ src/components/pages/SavingThrowsPane.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
 import '../../App.css';
 import './buildCharacter.css';
+import editIcon from '../images/icons8-quill-pen-64.png'
 
-export default function SavingThrowsPane() {
-  const [strThrow, setStrThrow] = useState(false);
-  const [dexThrow, setDexThrow] = useState(false);
-  const [conThrow, setConThrow] = useState(false);
-  const [intThrow, setIntThrow] = useState(false);
-  const [wisThrow, setWisThrow] = useState(false);
-  const [chaThrow, setChaThrow] = useState(false);
+export default function SavingThrowsPane({str, dex, con, int, wis, cha, getProfBonus}) {
+  const [strMod, strThrow, setStrThrow] = str;
+  const [dexMod, dexThrow, setDexThrow] = dex;
+  const [conMod, conThrow, setConThrow] = con;
+  const [intMod, intThrow, setIntThrow] = int;
+  const [wisMod, wisThrow, setWisThrow] = wis;
+  const [chaMod, chaThrow, setChaThrow] = cha;
 
+  const [editing, setEditing] = useState(false);
+  
   // Suggested implementation:
   // either use the input checkbox style directly or make a bool toggle
   // like the editButton and/or change the styling of the saving throw
@@ -17,55 +20,126 @@ export default function SavingThrowsPane() {
   // you need to calculate the mod for each save based on:
   // Base Ability Mod + profBonus() but only add prof if they are proficient
 
-  return <>
-    <h2 class="throwsTitle">Saving Throws</h2>
-    <input
-        type="text"
-        class="throws"
-        onChange={(e) => {
+  function SkillsTopBar() { 
+        const toggleEditing = () => {setEditing(!editing)};
+        return <div id="SavingThrowsTopBar">
+            <h2>Saving Throws</h2>
+            <img class="editButton" src={editIcon}
+                alt="Edit Abilities"
+                onClick={toggleEditing} />
+            </div>
+    }
+
+  function profBonus() {
+    
+  }
+
+  function getCheck() {
+    var checkBox = document.getElementById("myCheck");
+  }
+  
+  function SkillsCheckbox()  {
+    return<>
+
+    <> 
+      <input
+        type="checkbox"
+        class="checks"
+      />
+      {editing ? <input
+          type="text"
+          class="throws"
+          onChange={(e) => {
             setStrThrow(e.target.value);
+          }}                    
+          />: <text></text> }
+      STR<br />
+      
+    <input
+        type="checkbox"
+        class="checks"
+        onChange={(e) => {
+            setDexThrow(e.target.value);
         }}
     />
-    STR<br />
     <input
         type="text"
         class="throws"
         onChange={(e) => {
             setDexThrow(e.target.value);
         }}
+    />DEX<br />
+      
+    <input
+        type="checkbox"
+        class="checks"
+        onChange={(e) => {
+            setConThrow(e.target.value);
+        }}
     />
-    DEX<br />
     <input
         type="text"
         class="throws"
         onChange={(e) => {
             setConThrow(e.target.value);
         }}
-    />
-    CON <br />
+    />CON<br />
+      
+    <input
+        type="checkbox"
+        class="checks"
+        onChange={(e) => {
+            setIntThrow(e.target.value);
+        }}
+    />    
     <input
         type="text"
         class="throws"
         onChange={(e) => {
             setIntThrow(e.target.value);
         }}
+    />INT<br />
+      
+    <input
+        type="checkbox"
+        class="checks"
+        onChange={(e) => {
+            setWisThrow(e.target.value);
+        }}
     />
-    INT<br />
     <input
         type="text"
         class="throws"
         onChange={(e) => {
             setWisThrow(e.target.value);
         }}
+    />WIS<br />
+
+      
+    <input
+        type="checkbox"
+        class="checks"
+        onChange={(e) => {
+            setChaThrow(e.target.value);
+        }}
     />
-    WIS<br />
     <input
         type="text"
         class="throws"
         onChange={(e) => {
             setChaThrow(e.target.value);
         }}
-    />
-    CHA<br /><br />
-  </>
+    />CHA<br />      
+    </>
+      
+    </>
+  }
+
+  
+  
+  return <div id="SavingThrowsPane">
+    <SkillsTopBar/>
+    <SkillsCheckbox/>
+  <br />
+  </div>
 }
