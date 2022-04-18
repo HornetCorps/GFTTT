3d220a731189e8a2db999f26c401fdda8d1ede7b
alaurente21 <74069975+alaurente21@users.noreply.github.com>
2022-04-17 22:49:39 -0700

    GFTTT-108 Authentication System :+1:
    Username and password logs into firebase

--- src/components/pages/register.js
+++ src/components/pages/register.js
@@ -2,51 +2,29 @@ import '../../App.css';
 import './register.css';
 import React, { useState } from 'react';
 import { useNavigate } from "react-router";
+import {auth} from "../../firebase-config";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 
 export default function Register() {
     const navigate = useNavigate();
     
-    const [email, setEmailReg] = useState();
-    const [username, setUsernameReg] = useState();
-    const [password, setPasswordReg] = useState();
+    const [email, setEmailReg] = useState("");
+    const [username, setUsernameReg] = useState("");
+    const [password, setPasswordReg] = useState("");
+
     
-    async function onSubmit(e) {
-        let newUser = {email, username,password};
-        e.preventDefault();
-
-        if(email == null || username == null || password == null){
-            window.alert("All fields required.");
-            return;
+    async function signUp(e) {
+        try {
+            const user = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+            );
+            console.log(user);
+        } catch (error) {
+            console.log(error.message);
         }
-
-        if(password.length <= 8){
-            window.alert("Passwords must be at least 8 characters");
-            return;
-        }
-
-        await fetch("http://localhost:5000/api/register", {
-          method: "POST",
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(newUser),
-        })
-        .then(res => {
-            if(res.status === 422){
-                window.alert("User already exists");
-                navigate("/login");
-            }
-            if(res.status === 200){
-                window.alert("Registation successful");
-                navigate("/profile");
-            }
-        })
-        .catch(error => {
-          window.alert(error);
-          return;
-        });
-    }
+    };
 
     return <div className='register'>
     <h1>Registration</h1>
@@ -58,14 +36,6 @@ export default function Register() {
                 setEmailReg(e.target.value);
             }}
         />
-        <label>Username</label>
-        <input 
-            type="text" 
-            placeholder="Username"
-            onChange={(e) => {
-                setUsernameReg(e.target.value);
-            }}
-        />
         <label>Password</label>
         <input 
             type="password" 
@@ -74,6 +44,6 @@ export default function Register() {
                 setPasswordReg(e.target.value);
             }}
         />
-        <button onClick={onSubmit}>Register</button>
+        <button onClick={signUp}>Register</button>
     </div>
 }
