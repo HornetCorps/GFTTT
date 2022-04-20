a959f942f6806c612930227592c8ba319f451d02
Sean <sean111646@gmail.com>
2022-04-19 23:25:29 -0700

    GFTTT-105 Fixed Saving Throws, needs alignment

--- src/components/pages/SavingThrowsPane.js
+++ src/components/pages/SavingThrowsPane.js
@@ -1,26 +1,22 @@
 import React, {useState} from 'react';
 import '../../App.css';
 import './buildCharacter.css';
+import profNone from '../images/rank-one.png';
+import profFull from '../images/rank-four.png';
 import editIcon from '../images/icons8-quill-pen-64.png'
 
 export default function SavingThrowsPane({str, dex, con, int, wis, cha, getProfBonus}) {
-  const [strMod, strThrow, setStrThrow] = str;
-  const [dexMod, dexThrow, setDexThrow] = dex;
-  const [conMod, conThrow, setConThrow] = con;
-  const [intMod, intThrow, setIntThrow] = int;
-  const [wisMod, wisThrow, setWisThrow] = wis;
-  const [chaMod, chaThrow, setChaThrow] = cha;
-
+  const savingThrows = [
+    {name: "Strength", mod: str[0], val: str[1], updater: str[2]},
+    {name: "Dexterity", mod: dex[0], val: dex[1], updater: dex[2]},
+    {name: "Constitution", mod: con[0], val: con[1], updater: con[2]},
+    {name: "Intelligence", mod: int[0], val: int[1], updater: int[2]},
+    {name: "Wisdom", mod: wis[0], val: wis[1], updater: wis[2]},
+    {name: "Charisma", mod: cha[0], val: cha[1], updater: cha[2]},
+  ];
   const [editing, setEditing] = useState(false);
-  
-  // Suggested implementation:
-  // either use the input checkbox style directly or make a bool toggle
-  // like the editButton and/or change the styling of the saving throw
-  // name to indicate proficiency
-  // you need to calculate the mod for each save based on:
-  // Base Ability Mod + profBonus() but only add prof if they are proficient
 
-  function SkillsTopBar() { 
+  function SavesTopBar() {
         const toggleEditing = () => {setEditing(!editing)};
         return <div id="SavingThrowsTopBar">
             <h2>Saving Throws</h2>
@@ -28,118 +24,40 @@ export default function SavingThrowsPane({str, dex, con, int, wis, cha, getProfB
                 alt="Edit Abilities"
                 onClick={toggleEditing} />
             </div>
-    }
-
-  function profBonus() {
-    
   }
 
-  function getCheck() {
-    var checkBox = document.getElementById("myCheck");
-  }
-  
-  function SkillsCheckbox()  {
-    return<>
+  function SaveItem(save)  {
+    return <div class='SavingThrow'>
 
-    <> 
-      <input
-        type="checkbox"
-        class="checks"
-      />
-      {editing ? <input
-          type="text"
-          class="throws"
-          onChange={(e) => {
-            setStrThrow(e.target.value);
-          }}                    
-          />: <text></text> }
-      STR<br />
-      
-    <input
-        type="checkbox"
-        class="checks"
-        onChange={(e) => {
-            setDexThrow(e.target.value);
-        }}
-    />
-    <input
-        type="text"
-        class="throws"
-        onChange={(e) => {
-            setDexThrow(e.target.value);
-        }}
-    />DEX<br />
-      
-    <input
-        type="checkbox"
-        class="checks"
-        onChange={(e) => {
-            setConThrow(e.target.value);
-        }}
-    />
-    <input
-        type="text"
-        class="throws"
-        onChange={(e) => {
-            setConThrow(e.target.value);
-        }}
-    />CON<br />
-      
-    <input
-        type="checkbox"
-        class="checks"
-        onChange={(e) => {
-            setIntThrow(e.target.value);
-        }}
-    />    
-    <input
-        type="text"
-        class="throws"
-        onChange={(e) => {
-            setIntThrow(e.target.value);
-        }}
-    />INT<br />
-      
-    <input
-        type="checkbox"
-        class="checks"
-        onChange={(e) => {
-            setWisThrow(e.target.value);
-        }}
-    />
-    <input
-        type="text"
-        class="throws"
-        onChange={(e) => {
-            setWisThrow(e.target.value);
-        }}
-    />WIS<br />
-
-      
-    <input
-        type="checkbox"
-        class="checks"
-        onChange={(e) => {
-            setChaThrow(e.target.value);
-        }}
-    />
-    <input
-        type="text"
-        class="throws"
-        onChange={(e) => {
-            setChaThrow(e.target.value);
-        }}
-    />CHA<br />      
-    </>
-      
-    </>
+      {editing
+        ? (save.val
+          ? <img class='saveThrowIcon'
+              onClick={(e) => {save.updater((v) =>(!v))}}
+              src={profFull}/>
+          : <img class='saveThrowIcon'
+              onClick={(e) => {save.updater((v) =>(!v))}}
+              src={profNone}/>)
+        : (save.val
+          ? <img class='saveThrowIcon' src={profFull}/>
+          : <img class='saveThrowIcon' src={profNone}/>)
+      }
+      <h3 class='saveThrowBonus'>
+        {(save.mod + ((save.val)?(getProfBonus()):(0))>0) ?? '+'}
+        {save.mod + ((save.val)?(getProfBonus()):(0))}
+      </h3>
+      <h3 class='saveThrowName'>{save.name}</h3>
+      </ div>
   }
 
-  
-  
+  function SavesList() {
+    return <div id='SavingThrowsList'>
+      {savingThrows.map((save) => (SaveItem(save)))}
+    </div>
+  }
+
   return <div id="SavingThrowsPane">
-    <SkillsTopBar/>
-    <SkillsCheckbox/>
+    <SavesTopBar />
+    <SavesList />
   <br />
   </div>
 }
